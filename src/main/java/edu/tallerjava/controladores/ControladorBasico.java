6e90a4e7afb7877b37d972fb761e0a21da30bb99
Sebastian Ismael <sebastian.ismael@grupoesfera.com.ar>
2019-07-25 11:32:03 -0300

    agrega custom log

--- src/main/java/edu/tallerjava/controladores/ControladorBasico.java
+++ src/main/java/edu/tallerjava/controladores/ControladorBasico.java
@@ -1,5 +1,7 @@
 package edu.tallerjava.controladores;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ControladorBasico {
 
+    private static Logger logger = LoggerFactory.getLogger(ControladorBasico.class);
+
     @GetMapping("/saludar/{nombre}")
     public Saludo saludar(@PathVariable("nombre") String nombre){
+        logger.info("saludando a " + nombre);
         return new Saludo(nombre, "Hola!");
     }
 
