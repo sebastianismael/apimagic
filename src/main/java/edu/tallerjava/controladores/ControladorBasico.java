501c00d0c00792b1783263955722bd2c7ea63f28
Sebastian Ismael <sebastian.ismael@grupoesfera.com.ar>
2019-07-25 11:09:45 -0300

    agrega pathvariable

--- src/main/java/edu/tallerjava/controladores/ControladorBasico.java
+++ src/main/java/edu/tallerjava/controladores/ControladorBasico.java
@@ -1,14 +1,15 @@
 package edu.tallerjava.controladores;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ControladorBasico {
 
-    @GetMapping("/saludar")
-    public Saludo saludar(){
-        return new Saludo("Seba", "Hola!");
+    @GetMapping("/saludar/{nombre}")
+    public Saludo saludar(@PathVariable("nombre") String nombre){
+        return new Saludo(nombre, "Hola!");
     }
 
 
