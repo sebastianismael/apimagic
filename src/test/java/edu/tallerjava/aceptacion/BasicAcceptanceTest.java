55860e1e92d17af67483a547c475023c8c8b331e
Sebastian Ismael <sebastian.ismael@grupoesfera.com.ar>
2019-07-29 14:59:15 -0300

    agrega una superclase para los test de acpetacion

--- src/test/java/edu/tallerjava/aceptacion/BasicAcceptanceTest.java
+++ src/test/java/edu/tallerjava/aceptacion/BasicAcceptanceTest.java
@@ -1,32 +1,11 @@
 package edu.tallerjava.aceptacion;
 
 import edu.tallerjava.modelo.Category;
-import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class BasicAcceptanceTest {
-
-    @LocalServerPort
-    private int port;
-    private String url;
-
-    @Autowired
-    private TestRestTemplate restTemplate;
-
-    @Before
-    public void init(){
-        url = "http://localhost:" + port;
-    }
+public class BasicAcceptanceTest extends AcceptanceTest{
 
     @Test
     public void smoke() {
--- src/test/java/edu/tallerjava/aceptacion/CategoriesAcceptanceTest.java
+++ src/test/java/edu/tallerjava/aceptacion/CategoriesAcceptanceTest.java
@@ -1,33 +1,12 @@
 package edu.tallerjava.aceptacion;
 
-import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class CategoriesAcceptanceTest {
-
-    @LocalServerPort
-    private int port;
-    private String url;
-
-    @Autowired
-    private TestRestTemplate restTemplate;
-
-    @Before
-    public void init(){
-        url = "http://localhost:" + port;
-    }
+public class CategoriesAcceptanceTest extends AcceptanceTest{
 
     @Test
     public void findAll() {
