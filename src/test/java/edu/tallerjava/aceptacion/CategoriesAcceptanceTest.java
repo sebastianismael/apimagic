afea6ee547c9a83efcafdf31a3cb1bedc7710508
Sebastian Ismael <sebastian.ismael@grupoesfera.com.ar>
2019-07-31 14:04:44 -0300

    agrega test para categoria invalida

--- src/test/java/edu/tallerjava/aceptacion/CategoriesAcceptanceTest.java
+++ src/test/java/edu/tallerjava/aceptacion/CategoriesAcceptanceTest.java
@@ -2,12 +2,20 @@ package edu.tallerjava.aceptacion;
 
 import edu.tallerjava.modelo.Category;
 import org.junit.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.jdbc.Sql;
 
 import java.util.List;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+//@Transactional
+// If your test is @Transactional, it will rollback the transaction at the end of each test method by default.
+// However, as using this arrangement with either RANDOM_PORT or DEFINED_PORT implicitly provides a real servlet environment,
+// HTTP client and server will run in separate threads, thus separate transactions.
+// Any transaction initiated on the server won’t rollback in this case.
 public class CategoriesAcceptanceTest extends AcceptanceTest{
 
     @Test
@@ -20,8 +28,21 @@ public class CategoriesAcceptanceTest extends AcceptanceTest{
     @Test
     @Sql(value = "/sql/createCategories.sql")
     public void getSingleCategory(){
-        final Category category = this.restTemplate.getForObject(url + "/categories/1", Category.class);
+        final List results = this.restTemplate.getForObject(url + "/categories", List.class);
+        String uri = url + "/categories/" + ((Map)results.get(0)).get("id");
+
+        final ResponseEntity<Category> responseEntity = this.restTemplate.getForEntity(uri, Category.class);
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+        final Category category = responseEntity.getBody();
         assertThat(category.getPermalink()).isEqualTo("http://home.mercadolibre.com.ar/vehiculos-accesorios/");
+    }
+
+    @Test
+    @Sql(value = "/sql/createCategories.sql")
+    public void getInvalidCategory(){
+        final ResponseEntity<Category> responseEntity = this.restTemplate.getForEntity(url + "/categories/9891", Category.class);
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
 }
