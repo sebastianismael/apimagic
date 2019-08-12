f0a5c4843c97adb053ebac4472ffb1685ad56260
Sebastian Ismael <sebastian.ismael@grupoesfera.com.ar>
2019-08-12 10:29:18 -0300

    agrega javadoc

--- src/test/java/edu/tallerjava/aceptacion/AcceptanceTest.java
+++ src/test/java/edu/tallerjava/aceptacion/AcceptanceTest.java
@@ -28,6 +28,13 @@ public abstract class AcceptanceTest {
         url = "http://localhost:" + port;
     }
 
+    /**
+     * Permite obtener como respuesta de un get un List tipado
+     * @param uri path del servicio
+     * @param responseType un wrapper de una lista parametrizada
+     * @param <T> el tipo de datos de los objetos de la lista resultado
+     * @return Una lista de objetos del tipo T
+     */
     protected <T> List<T> getForObject(String uri, ParameterizedTypeReference<List<T>> responseType) {
         return restTemplate.exchange(uri, HttpMethod.GET, null, responseType).getBody();
     }
