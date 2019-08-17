672b9c56fedf5199c7a1ffc572d39de4b3e4e3ee
Sebastian Ismael <sebastian.ismael@grupoesfera.com.ar>
2019-08-17 13:32:24 -0300

    alimina del dto el id poeque no se usa

--- src/main/java/edu/tallerjava/dto/CategoryDto.java
+++ src/main/java/edu/tallerjava/dto/CategoryDto.java
@@ -2,18 +2,9 @@ package edu.tallerjava.dto;
 
 public class CategoryDto {
 
-    private Long id;
     private String codigo;
     private String nombre;
     private String permalink;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 
     public String getCodigo() {
         return codigo;
