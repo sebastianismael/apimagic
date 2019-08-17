3beeaa87d68ca9791ebea9967589715cb7c20134
Sebastian Ismael <sebastian.ismael@grupoesfera.com.ar>
2019-08-17 10:50:33 -0300

    refactor para usar jpa query

--- src/main/java/edu/tallerjava/repositorios/CustomizedCategoryRepositoryImpl.java
+++ src/main/java/edu/tallerjava/repositorios/CustomizedCategoryRepositoryImpl.java
@@ -1,22 +1,22 @@
 package edu.tallerjava.repositorios;
 
 import edu.tallerjava.modelo.Category;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.EntityManager;
 import java.util.LinkedList;
 import java.util.List;
 
 @Repository
 public class CustomizedCategoryRepositoryImpl implements CustomizedCategoryRepository {
 
+    @Autowired
+    private EntityManager entityManager;
+
     public List<Category> findByCode(String code) {
-        List<Category> list = new LinkedList<>();
-
-        Category c1 = new Category();
-        c1.setCodigo("MLA5725");
-        c1.setNombre("Accesorios para Vehículos");
-        list.add(c1);
-
-        return list;
+        return entityManager.createQuery("select c from Category c where c.codigo =:code")
+                            .setParameter("code", code)
+                            .getResultList();
     }
 }
